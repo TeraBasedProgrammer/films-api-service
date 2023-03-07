8e8324935fda8012de6a9a9175ca64eef9311468
anton <anton.uvarenko@chisw.com>
2023-03-07 20:25:17 +0200

    feat(auth): added basic methods for communication with database and some field to users struct

--- authorization-service/internal/core/repo/entities/User.go
+++ authorization-service/internal/core/repo/entities/User.go
@@ -1,13 +1,22 @@
 package entities
 
-import "github.com/jmoiron/sqlx"
+import (
+	"context"
+	"github.com/jmoiron/sqlx"
+	"time"
+)
+
+const DefaultExecTimeout = 5 * time.Second
 
 type User struct {
-	Id       int    `json:"id"`
-	Username string `json:"username"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
-	Salt     string `json:"salt"`
+	Id        int       `json:"id"`
+	Avatar    string    `json:"avatar"`
+	Username  string    `json:"username"`
+	Email     string    `json:"email"`
+	Password  string    `json:"password"`
+	Salt      string    `json:"salt"`
+	CreatedAt time.Time `json:"-"`
+	UpdatedAt time.Time `json:"-"`
 }
 
 type UserRepo struct {
@@ -16,4 +25,44 @@ type UserRepo struct {
 
 func NewUserRepo(db *sqlx.DB) *UserRepo {
 	return &UserRepo{db: db}
+}
+
+func (r *UserRepo) AddUser(user *User) error {
+	ctx, cancel := context.WithTimeout(context.Background(), DefaultExecTimeout)
+	defer cancel()
+
+	query := `
+	INSERT INTO users (avatar_name, username, email, password, salt, created_at, updated_at)
+	VALUES (?, ?, ?, ?, ?, ?, ?)
+`
+	_, err := r.db.ExecContext(
+		ctx,
+		query,
+		user.Email,
+		user.Username,
+		user.Email,
+		user.Password,
+		user.Salt,
+		user.CreatedAt,
+		user.UpdatedAt,
+	)
+
+	return err
+}
+
+func (r *UserRepo) GetUserById(id int) (*User, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), DefaultExecTimeout)
+	defer cancel()
+
+	query := `
+	SELECT FROM users
+	WHERE id = ?
+`
+	user := &User{}
+	err := r.db.SelectContext(ctx, user, query, id)
+	if err != nil {
+		return nil, err
+	}
+
+	return user, nil
 }
