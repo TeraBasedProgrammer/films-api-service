8943dba9ec0343f9b42edf5ebb4e6d520c74bf13
Ilya <ilyadronov2332@gmail.com>
2023-03-21 14:03:07 +0200

    feat(films): Film model init version

--- films/entrypoint.sh
+++ films/entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sh -x
 
 if [ "$DATABASE" = "postgres" ]
 then
@@ -12,6 +12,7 @@ then
 fi
 
 python manage.py flush --no-input
+python manage.py makemigrations
 python manage.py migrate
 
 exec "$@"
--- films/films/models.py
+++ films/films/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Film(models.Model):
+
+    # Добавить базовую валидацию полей
+
+    title = models.CharField(max_length=50)
+    poster_name = models.CharField(max_length=100)
+    rating = models.DecimalField(max_digits=4, decimal_places=2)
+    country = models.CharField(max_length=50)
+    realese_date = models.DateField()
+    director = models.CharField(max_length=50)
+    description = models.TextField()
+    age_restriction = models.IntegerField()
+    studio = models.CharField(max_length=50)
+    
+    # imdb_film_id - cделать write_only field в сериализаторе, запрашивать fild + валидация на 'tt' в начале
+    # imdb_rating  - cделать дополнительным полем в сериализаторе (парсить json-ответ от imdb)
+    # actors - дополнительное поле в сериализаторе (делать запрос на сторонний API)
+
+class Screenshot(models.Model):
+    film = models.ForeignKey(Film, on_delete=models.CASCADE, related_name='screenshots')
+    name = models.CharField(max_length=100)
