afe944d15b9f04eb0a52524e8b2b06445f7d2596
Ilya <ilyadronov2332@gmail.com>
2023-05-30 11:29:08 +0300

    fix(films): fixed update / delete endpoints for models (added slashes)

--- films/actors/urls.py
+++ films/actors/urls.py
@@ -5,7 +5,7 @@ from .views import *
 urlpatterns = [
     path('', actor_list_create),
     path('<int:pk>/', actor_retrieve, name='actor_retrieve'),
-    path('<int:pk>/update', actor_update),
-    path('<int:pk>/delete', actor_delete),
+    path('<int:pk>/update/', actor_update),
+    path('<int:pk>/delete/', actor_delete),
     path('search/', actor_search),
 ]
