bf051921510fdad479781f114507fbe03ed2f1d6
Ilya <ilyadronov2332@gmail.com>
2023-06-30 19:35:43 +0300

    fix:reset-db script refactor and some small changes

--- films/backend/settings.py
+++ films/backend/settings.py
@@ -75,8 +75,8 @@ DATABASES = {
         "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
         "NAME": os.environ.get("SQL_DATABASE", os.path.join(BASE_DIR, "db.sqlite3")),
         "USER": os.environ.get("SQL_USER"),
-        "PASSWORD": os.environ.get('SQL_PASSWORD') if DEBUG else os.environ.get("SQL_PROD_PASSWORD"),
-        "HOST": os.environ.get('SQL_HOST') if DEBUG else os.environ.get("SQL_PROD_HOST"),
+        "PASSWORD": os.environ.get('SQL_PASSWORD'), # if DEBUG else os.environ.get("SQL_PROD_PASSWORD"),
+        "HOST": os.environ.get('SQL_HOST'), # if DEBUG else os.environ.get("SQL_PROD_HOST"),
         "PORT": os.environ.get("SQL_PORT", "5432"),
     }
 }
--- films/films/management/commands/reset-db.py
+++ films/films/management/commands/reset-db.py
@@ -1,8 +1,6 @@
-from django.conf import settings
 from django.core.management.base import BaseCommand
-
-# import films.models as models
-from films.models import Film
+from django.conf import settings
+import films.models as models
 from films.services import clean_s3_model_data
 
 import logging
@@ -14,15 +12,19 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
 
         # Retrieving all model classes of the project
-        # models_classes = dict([(name, cls) for name, cls in models.__dict__.items() if isinstance(cls, type)])
+        models_classes = dict([(name, cls) for name, cls in models.__dict__.items() if isinstance(cls, type)])
 
         # Cleaning all project's custom models (including s3 data)
-        # for key, value in models_classes.items():
-        instances = Film.objects.all()
+        for key, value in models_classes.items():
+            try:
+                instances = value.objects.all()
+                if not settings.DEBUG:
+                    for instance in instances:
+                        clean_s3_model_data(instance) 
 
-        if not settings.DEBUG:
-            for instance in instances:
-                clean_s3_model_data(instance)
+                deleted_count, _ = instances.delete()
+                logger.debug(f'Cleaned model "{key}". Objects deleted: {deleted_count}')
+            except AttributeError:
+                pass
 
-        deleted_count, _ = instances.delete()
-        logger.debug(f'Cleaned model "Film". Objects deleted: {deleted_count}')
+
